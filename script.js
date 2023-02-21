2834ecbb5c4cc2e836c999f4172ba0a72987065b
Dasha <missis.dashulka@inbox.ru>
2023-02-21 18:34:34 +0300

    tasks-4

--- script.js
+++ script.js
@@ -1,20 +1,15 @@
-let out = '';
-let w = 3;
-let q = 7;
+fetch('https://api.openweathermap.org/data/2.5/weather?q=Minsk&appid=df068345397ee8c206cc4d1be611ff1c')
+   .then(function (resp) { return resp.json() })
+   .then(function (data) {
+      console.log(data);
+      document.querySelector('.city').innerHTML = data.name;
+      document.querySelector('.degree').innerHTML = Math.round(data.main.temp - 273) + '&deg;';
+      //https://openweathermap.org/img/wn/13d@2x.png
+      document.querySelector('.feature').innerHTML = `<img src="https://openweathermap.org/img/wn/${data.weather[0]['icon']}@2x.png">`;
+      document.querySelector('.weather').innerHTML = data.weather[0]['description'];
+      document.querySelector('.gust li').innerHTML = data.wind.gust;
+      document.querySelector('.speed li').innerHTML = data.wind.speed;
+   })
+   .catch(function () {
 
-for (let i = 0; i < 4; i++) {
-   for (let k = 0; k < 8; k++) {
-      if (k < w || k > q) {
-         //out += '- ';
-         out += '&nbsp;&nbsp;';
-      }
-      else {
-         out += '*';
-      }
-   }
-   w--;
-   q--;
-   out += '<br>'
-}
-
-document.querySelector('.out').innerHTML = out;
+   });
