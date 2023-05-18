5015cde3a16815466acc45592e3ae95325489ba0
Dasha <missis.dashulka@inbox.ru>
2023-05-18 16:50:55 +0300

    Add closure

--- script.js
+++ script.js
@@ -1,28 +1,7 @@
-field.onclick = function (event) {
-   // координаты поля относительно окна браузера
-   let fieldCoords = this.getBoundingClientRect();
+let panes = document.querySelectorAll('.pane');
 
-   let ballCoords = {
-      top: event.clientY - fieldCoords.top - field.clientTop - ball.clientHeight / 2,
-      left: event.clientX - fieldCoords.left - field.clientLeft - ball.clientWidth / 2
-   };
-
-   // запрещаем пересекать верхнюю границу поля
-   if (ballCoords.top < 0) ballCoords.top = 0;
-
-   // запрещаем пересекать левую границу поля
-   if (ballCoords.left < 0) ballCoords.left = 0;
-
-   // запрещаем пересекать правую границу поля
-   if (ballCoords.left + ball.clientWidth > field.clientWidth) {
-      ballCoords.left = field.clientWidth - ball.clientWidth;
-   }
-
-   // запрещаем пересекать нижнюю границу поля
-   if (ballCoords.top + ball.clientHeight > field.clientHeight) {
-      ballCoords.top = field.clientHeight - ball.clientHeight;
-   }
-
-   ball.style.left = ballCoords.left + 'px';
-   ball.style.top = ballCoords.top + 'px';
+for (let pane of panes) {
+   pane.insertAdjacentHTML("afterbegin", '<button class="remove-button">[x]</button>');
+   // кнопка становится первым потомком плитки (pane)
+   pane.firstChild.onclick = () => pane.remove();
 }
